b43eeb6282d763de9e44ea263cbb405cd8e53f26
Moon <114739237+MOONCoreDev@users.noreply.github.com>
2023-12-22 11:23:43 +0700

    update slaves.lua

--- slaves.lua
+++ slaves.lua
@@ -1,1 +1,28 @@
+local NEVERLOSE = loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/NEVERLOSE-UI-Nightly/main/source.lua"))()
 
+NEVERLOSE:Theme("original")
+
+local Window = NEVERLOSE:AddWindow(randomString(),"ใช้แรงงานผึ้ง Simulator")
+local Notification = NEVERLOSE:Notification()
+
+Notification.MaxNotifications = 6
+local createnotification = function(title, text, delay)
+	title = title or "Notification"
+	text = text or "Test Notification"
+	delay = delay
+	Notification:Notify("info", title, text, delay)
+end
+local createwarning = function(title, text, delay)
+	title = title or "Notification"
+	text = text or "Test Notification"
+	delay = delay
+	Notification:Notify("warning", title, text, delay)
+end
+
+Window:AddTabLabel('Main')
+
+local Main = Window:AddTab('Main','ads')
+local addSection = function(name,state)
+	state = state or 'left'
+	return Main:AddSection(name,state)
+end
